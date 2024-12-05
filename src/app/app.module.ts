2fff353349f763b6799f056ee0733e1f78082c16
Aimerick-Noua <aimericknoua4@gmail.com>
2024-12-05 18:11:37 +0100

    created module communication and shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,20 @@ import { TasksComponent } from "./tasks/tasks.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { TaskComponent } from "./tasks/task/task.component";
 import { NewTaskComponent } from "./tasks/new-task/new-task.component";
-import { CardComponent } from "./shared/card/card.component";
 import { FormsModule } from "@angular/forms";
-import { CommonModule } from "@angular/common";
+import { SharedModule } from "./shared/shared.module";
+import { TasksModule } from "./tasks/tasks.module";
 
 @NgModule({
     declarations: [
         AppComponent,
         HeaderComponent,
         UserComponent,
-        TasksComponent,
-        TaskComponent,
-        NewTaskComponent,
-        CardComponent
     ],
     imports: [
-        FormsModule,
-        BrowserModule
+        BrowserModule,
+        SharedModule,
+        TasksModule
         ],
     bootstrap: [AppComponent]
 })
